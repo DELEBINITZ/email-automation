ccd48a2f1b1557a8bec8182143f021e26ea14002
akshayTupe00 <akshay.tupe@kirana.club>
2024-06-23 05:32:22 +0530

    added error handler

---
+++ index.js
@@ -0,0 +1,55 @@
+const express = require("express");
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+
+// import environment variables
+require("dotenv").config();
+
+const app = express();
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  if (req.method === "OPTIONS") {
+    res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
+    return res.status(200).json({});
+  }
+  next();
+});
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use(morgan("tiny"));
+
+
+app.get("/", (req, res) => {
+  res.status(200).send("API is live !");
+});
+
+app.use((req, res, next) => {
+  const error = new Error("Not Found");
+  error.status = 404;
+  next(error);
+});
+
+// Handle Error thrown by API
+app.use((error, req, res, next) => {
+  console.log(error.message);
+  res.status(error.status || 500);
+  res.json({
+    error: {
+      message: error.message,
+    },
+  });
+});
+
+app.listen(process.env.SERVER_PORT, () => {
+  console.log("server running on port:", process.env.SERVER_PORT);
+  if (process.env.SERVER_PORT === "80") {
+    // PRODUCTION DEPLOYMENT
+  }
+});
